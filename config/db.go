7c423b19c641ca56370ff3b8505a7d3a0a6fbcc3
testgithubanjali <131359262+testgithubanjali@users.noreply.github.com>
2026-05-09 22:14:52 +0530

    add: added code inside db.go

---
+++ config/db.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var DB *mongo.Database
+
+func ConnectDB(uri string) {
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	DB = client.Database("taskmanager")
+
+	log.Println("MongoDB Connected")
+}
