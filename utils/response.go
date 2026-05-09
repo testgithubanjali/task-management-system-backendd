5efb30142a6149e8fa1a19591e0d06a8688ec475
testgithubanjali <131359262+testgithubanjali@users.noreply.github.com>
2026-05-09 22:32:02 +0530

    add: added code ininside response.go

---
+++ utils/response.go
@@ -0,0 +1,28 @@
+package utils
+
+import "github.com/gin-gonic/gin"
+
+type APIResponse struct {
+	Success bool        `json:"success"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
+	Error   interface{} `json:"error,omitempty"`
+}
+
+func SuccessResponse(c *gin.Context, statusCode int, message string, data interface{}) {
+
+	c.JSON(statusCode, APIResponse{
+		Success: true,
+		Message: message,
+		Data:    data,
+	})
+}
+
+func ErrorResponse(c *gin.Context, statusCode int, message string, err interface{}) {
+
+	c.JSON(statusCode, APIResponse{
+		Success: false,
+		Message: message,
+		Error:   err,
+	})
+}
