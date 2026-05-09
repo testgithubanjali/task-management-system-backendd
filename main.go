c00041a46f65e92ef728d0efd3e52b5403d7dd94
testgithubanjali <131359262+testgithubanjali@users.noreply.github.com>
2026-05-09 22:16:26 +0530

    add: added code inside main.go

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"backend/config"
+	"backend/routes"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env")
+	}
+
+	config.ConnectDB(os.Getenv("MONGO_URI"))
+
+	router := gin.Default()
+
+	routes.AuthRoutes(router)
+	routes.TaskRoutes(router)
+
+	router.Run(":" + os.Getenv("PORT"))
+}
